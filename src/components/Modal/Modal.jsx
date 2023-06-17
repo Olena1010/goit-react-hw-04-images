dbf052bb5dc88c978a5dd0932f1e68a0d2f7cdee
Olena1010 <ebojcenko55@gmail.com>
2023-06-17 16:19:24 +0300

    Updates modal with hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,53 +1,49 @@
-import { Component } from 'react';
-import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import { IconContext } from 'react-icons';
 import { MdClose } from 'react-icons/md';
-import { Overlay, Wrapper, Image, CloseButton } from './Modal.styled';
+import { CloseButton, Image, Overlay, Wrapper } from './Modal.styled';
 
 const ModalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  static propTypes = {
-    url: PropTypes.string.isRequired,
-    onClose: PropTypes.func.isRequired,
-  };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscKeydown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscKeydown);
-  }
-
-  handleOverlayClick = ({ target, currentTarget }) => {
+export const Modal = ({ url, onClose }) => {
+  const handleOverlayClick = ({ target, currentTarget }) => {
     if (target === currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleEscKeydown = ({ code }) => {
-    if (code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+  useEffect(() => {
+    const handleEscKeydown = ({ code }) => {
+      if (code === 'Escape') {
+        onClose();
+      }
+    };
 
-  render() {
-    const { url, onClose } = this.props;
+    window.addEventListener('keydown', handleEscKeydown);
 
-    return createPortal(
-      <Overlay onClick={this.handleOverlayClick}>
-        <Wrapper>
-          <CloseButton type="button" onClick={onClose}>
-            <IconContext.Provider value={{ size: '2em' }}>
-              <MdClose />
-            </IconContext.Provider>
-          </CloseButton>
-          <Image src={url} alt="" />
-        </Wrapper>
-      </Overlay>,
-      ModalRoot
-    );
-  }
-}
+    return () => {
+      window.removeEventListener('keydown', handleEscKeydown);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <Wrapper>
+        <CloseButton type="button" onClick={onClose}>
+          <IconContext.Provider value={{ size: '2em' }}>
+            <MdClose />
+          </IconContext.Provider>
+        </CloseButton>
+        <Image src={url} alt="" />
+      </Wrapper>
+    </Overlay>,
+    ModalRoot
+  );
+};
+
+Modal.propTypes = {
+  url: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
